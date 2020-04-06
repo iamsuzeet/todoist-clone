55801a1586e26eef19661310b9d731852d7f8970
iamsuzeet <iamsuzeet@gmail.com>
2020-04-06 18:00:18 +0545

    sidebar component for viewing projects

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   FaChevronDown,
   FaInbox,
@@ -6,35 +6,83 @@ import {
   FaRegCalendar,
 } from 'react-icons/fa';
 
-export const Sidebar = () => (
-  <div className="sidebar" data-testid="sidebar">
-    <ul className="sidebar__generic">
-      <li>
+import { useSelectedProjectValue } from './../../context/selected-project-context';
+import { Projects } from './../Projects';
+import { AddProject } from './../AddProject';
+
+export const Sidebar = () => {
+  const { setSelectedProject } = useSelectedProjectValue();
+  const [active, setActive] = useState('inbox');
+  const [showProjects, setShowProjects] = useState(true);
+
+  return (
+    <div className="sidebar" data-testid="sidebar">
+      <ul className="sidebar__generic">
+        <li
+          data-testid="inbox"
+          className={active === 'inbox' ? 'active' : undefined}
+        >
+          <div
+            data-testId="inbox-action"
+            onClick={() => {
+              setActive('inbox');
+              setSelectedProject('INBOX');
+            }}
+          >
+            <span>
+              <FaInbox />
+            </span>
+            <span>Inbox</span>
+          </div>
+        </li>
+        <li
+          data-testid="today"
+          className={active === 'today' ? 'active' : undefined}
+        >
+          <div
+            data-testId="today-action"
+            onClick={() => {
+              setActive('today');
+              setSelectedProject('TODAY');
+            }}
+          >
+            <span>
+              <FaRegCalendar />
+            </span>
+            <span>Today</span>
+          </div>
+        </li>
+        <li
+          data-testid="next_7"
+          className={active === 'next_7' ? 'active' : undefined}
+        >
+          <div
+            data-testId="inbox-action"
+            onClick={() => {
+              setActive('next_7');
+              setSelectedProject('NEXT_7');
+            }}
+          >
+            <span>
+              <FaRegCalendarAlt />
+            </span>
+            <span>Next 7 days</span>
+          </div>
+        </li>
+      </ul>
+      <div
+        className="sidebar__middle"
+        onClick={() => setShowProjects(!showProjects)}
+      >
         <span>
-          <FaInbox />
+          <FaChevronDown
+            className={!showProjects ? 'hidden-projects' : undefined}
+          />
         </span>
-        Inbox
-      </li>
-      <li>
-        <span>
-          <FaRegCalendar />
-        </span>
-        Today
-      </li>
-      <li>
-        <span>
-          <FaRegCalendarAlt />
-        </span>
-        Next 7 days
-      </li>
-    </ul>
-    <div className="sidebar__middle">
-      <span>
-        <FaChevronDown />
-      </span>
-      <h2>Project</h2>
+        <h2>Project</h2>
+      </div>
+      <ul className="sidebar__projects">{showProjects && <Projects />}</ul>
+      {showProjects && <AddProject />}
     </div>
-    <ul className="sidebar__projects">Projects will be here!</ul>
-    Add Project component!
-  </div>
-);
+  );
+};
