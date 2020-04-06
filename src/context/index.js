88404d8a10b0469885c91b380a97fdc6edefccbe
iamsuzeet <iamsuzeet@gmail.com>
2020-04-06 17:53:35 +0545

    import source of project and selected project contex

---
+++ src/context/index.js
@@ -0,0 +1,20 @@
+import {
+  ProjectsContext,
+  ProjectsProvider,
+  useProjectsValue,
+} from './projects-context';
+
+import {
+  selectedProjectContext,
+  SelectedProjectProvider,
+  useSelectedProjectValue,
+} from './selected-project-context';
+
+export {
+  ProjectsContext,
+  ProjectsProvider,
+  useProjectsValue,
+  selectedProjectContext,
+  SelectedProjectProvider,
+  useSelectedProjectValue,
+};
